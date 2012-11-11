317bf7c46b080d968a04f068c053ac5a1533bdf1
Rian Rainey <rian@rianrainey.com>
2012-11-11 15:29:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CfwnApp::Application.config.secret_token = 'c09c1396a508857f7f1830fb5f07cccc7059eef83e26178d3cc79ae5b0288223b079ff5b21828e5dfa1c6cfd3d41b191a1af554fee551c8a49da72d4dc230bd9'
